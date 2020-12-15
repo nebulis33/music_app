3f48f61611bd7a180b019163d5457b24afee2248
nebulis33 <>
2020-12-15 15:23:28 -0800

    added basic stying and some helpers

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,11 @@ end
 
 # Tracks
 for i in (1..4) do
-    Track.create(title: Faker::Music::GratefulDead.unique.song, ord: i, album_id: 1)
+    Track.create(title: Faker::Music::GratefulDead.unique.song, ord: i, album_id: 1, lyrics: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 end
 for i in (5..8) do
     Track.create(title: Faker::Music::GratefulDead.unique.song, ord: i, regular: false, album_id: 1)
@@ -40,3 +44,6 @@ end
 for i in (5..8) do
     Track.create(title: Faker::Music::GratefulDead.unique.song, ord: i, regular: false, album_id: 4)
 end
+
+# Notes
+Note.create(user_id: 1, track_id: 1, body: "Lorem ipsum notes.")
